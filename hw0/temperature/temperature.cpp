b4a9f1f4e0a9d64b758c6083d29737ca9420ec9a
Solomon Himelbloom <2001solomon@gmail.com>
2021-01-21 22:25:10 -0900

    add: hw0 (initial files)

---
+++ hw0/temperature/temperature.cpp
@@ -0,0 +1,18 @@
+// temperature.cpp
+// Solomon Himelbloom
+// 21 January 2021
+// Temperature conversion program for CS 202.
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main() {
+    cout << "Hello, Temperature Conversion." << endl;
+
+    cout << "Press ENTER to quit... ";
+    while (cin.get() != '\n') ;
+
+    return 0;
+}
