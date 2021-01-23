bf3b10d28cc261548ab9b035517b5ff5eb6b1022
Solomon Himelbloom <2001solomon@gmail.com>
2021-01-22 22:23:34 -0900

    add: temperature.cpp unit conversions

--- hw0/temperature/temperature.cpp
+++ hw0/temperature/temperature.cpp
@@ -9,18 +9,31 @@ using std::cout;
 using std::endl;
 using std::cin;
 using std::stod;
+using std::strtod;
+using std::string;
 
 // Conversion using the C++ Standard Template Library (stod).
-double cpp_ftoc(const char* str) {
-    return 0;
+double cpp_ftoc(const char* new_str) {
+    double temperature = 0;
+    string str(new_str);
+    temperature = stod(new_str);
+
+    // Fahrenheit --> Celsius
+    temperature = ((5/9) * ((temperature + 40) - 40));
+    return temperature;
 }
 
 // Function using a C function (strod) for conversions.
-double c_ctof(const char*str) {
-    return 0;
+double c_ctof(const char* old_str) {
+    double temperature = 0;
+    temperature = strtod(old_str, nullptr);
+
+    // Celsius --> Fahrenheit
+    temperature = ((9/5) * ((temperature + 40) - 40)); 
+    return temperature;
 }
 
-int main(int argc, const char ** argv) {
+int main(int argc, char ** argv) {
     cout << argc << " total arguments, program name is " << argv[0] << "\n";
 
     return 0;
