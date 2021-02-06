934c32bd3a1c0cb0bcf378749a57ff21bc3f50b0
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2021-02-05 21:46:00 -0900

    add: file I/O & Project Gutenberg
    Source: https://www.gutenberg.org/

--- hw1/time_it_two/main.cpp
+++ hw1/time_it_two/main.cpp
@@ -12,6 +12,8 @@
 #include <random>
 #include <chrono>
 #include <ctime>
+#include <fstream>
+#include <sstream>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -22,9 +24,48 @@ using std::reverse;
 using std::mt19937;
 using std::random_device;
 using std::uniform_int_distribution;
+using std::ifstream;
+using std::ofstream;
+using std::istringstream;
+
+// Read a local file and print contents to the console.
+bool readFile(const string &filename) {
+    ifstream file(filename);
+
+    while (true) {
+        string new_line;
+        getline(file, new_line);
+
+        if (!file) {
+            if (file.eof()) {
+                std::cout << "Finished reading the requested file." << std::endl;
+                return true;
+            }
+            else {
+                std::cout << "Error during transit. Please try again." << std::endl;
+                return false;
+            }
+            break;
+        }
+        std::cout << new_line << std::endl;
+    }
+
+    if (!file) {
+        std::cout << "Cannot open the requested file." << std::endl;
+        return false;
+    }
+
+    cout << filename;
+    return true;
+}
 
 int main() {
     cout << "Hello, Time It (#2)." << endl;
+    
+    const string fname = "input.txt";  // Example input file.
+    const string book = "books/wonderland.txt";  // Designated file to read and write.
+
+    bool readsuccess = readFile(fname);
 
     cout << "Press ENTER to quit... ";
     while (cin.get() != '\n') ;
