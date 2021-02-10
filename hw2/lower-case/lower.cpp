76ddef487e1c4b6576b6ccf11b4378313d114c84
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2021-02-09 23:24:41 -0900

    add: hw2 (initial files)

---
+++ hw2/lower-case/lower.cpp
@@ -0,0 +1,28 @@
+// lower.cpp
+// Solomon Himelbloom
+// 9 February 2021
+// Lowercase letters example for CS 202.
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fstream;
+using std::ifstream;
+using std::map;
+using std::ofstream;
+using std::reverse;
+using std::sort;
+using std::string;
+using std::vector;
+
+int main() {
+    cout << "Hello, Lower Case Letters." << endl;
+}
---
+++ hw2/memory-layout/memory.cpp
@@ -0,0 +1,28 @@
+// memory.cpp
+// Solomon Himelbloom
+// 9 February 2021
+// Memory layout example for CS 202.
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fstream;
+using std::ifstream;
+using std::map;
+using std::ofstream;
+using std::reverse;
+using std::sort;
+using std::string;
+using std::vector;
+
+int main() {
+    cout << "Hello, Memory Layout." << endl;
+}
