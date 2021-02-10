4015b737cad6f94cb6ec1fae5bf0ae7a352d43ee
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2021-02-09 23:52:31 -0900

    add: Catch2 testing for linked list

--- hw2/linked-list/list.cpp
+++ hw2/linked-list/list.cpp
@@ -1,8 +1,9 @@
 // list.cpp
 // Solomon Himelbloom
 // 9 February 2021
-// Linked list example for CS 202.
+// Linked list (source file) example for CS 202.
 
+#include "catch.hpp"
 #include <iostream>
 #include <stdio.h>
 #include <string>
@@ -11,6 +12,7 @@
 #include <algorithm>
 #include <fstream>
 #include <iomanip>
+#include <list>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -22,7 +24,14 @@ using std::reverse;
 using std::sort;
 using std::string;
 using std::vector;
+using std::list;
 
-int main() {
-    cout << "Hello, Linked List." << endl;
+unsigned int Cubed(unsigned int number) {
+    return number < 1 ? 1 : number*number*number;
+}
+
+TEST_CASE("Third powers are checked", "[cubed]") {
+    REQUIRE(Cubed(3) == 27);
+    REQUIRE(Cubed(4) == 64);
+    REQUIRE(Cubed(5) == 125);
 }
