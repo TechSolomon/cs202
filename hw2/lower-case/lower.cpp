5e6c0c9671cdf761ac8a5cf122339097168223d0
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2021-02-12 21:30:55 -0900

    add: case sensitivity analysis (lower.cpp)

--- hw2/lower-case/lower.cpp
+++ hw2/lower-case/lower.cpp
@@ -8,9 +8,26 @@
 // Replaces all uppercase characters in the 
 // C-style string 's' with their lowercase equivalents.
 void to_lower(char* s) {
-    
+    for (;*s;s++) {
+        if ('A' <= *s && 'Z' >= *s) {
+            *s -= ('A'-'a');
+        }
+    } if (s==0) {
+        return;
+    }
+}
+
+// Compare above to_lower function input to desired output.
+void analysis(const std::string& t) {
+    std::string testing = t;
+    std::cout << testing << std::endl;
+    char* s = &testing[0];
+    to_lower(s);
+    std::cout << s << std::endl;
 }
 
 int main() {
-    std::cout << "Hello, Lower Case Letters." << std::endl;
+    analysis("Hello, World!");
+    analysis("Solomon was here.");
+    analysis("UAF-CS");
 }
