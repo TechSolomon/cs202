c2ed913cf4688f3d7992b864117ed08c2ef7e41b
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2021-02-27 22:10:02 -0900

    add: hw3 (initial files)

---
+++ hw3/book/prettyprinter.cpp
@@ -0,0 +1,28 @@
+// prettyprinter.cpp
+// Solomon Himelbloom
+// 27 February 2021
+// Book pretty printer example for CS 202.
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fstream;
+using std::ifstream;
+using std::map;
+using std::ofstream;
+using std::reverse;
+using std::sort;
+using std::string;
+using std::vector;
+
+int main() {
+    cout << "Hello, Book Pretty Printer." << endl;
+}
---
+++ hw3/tokenizer/tokenizer.cpp
@@ -0,0 +1,28 @@
+// tokenizer.cpp
+// Solomon Himelbloom
+// 27 February 2021
+// Simple tokenizer example for CS 202.
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <iomanip>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fstream;
+using std::ifstream;
+using std::map;
+using std::ofstream;
+using std::reverse;
+using std::sort;
+using std::string;
+using std::vector;
+
+int main() {
+    cout << "Hello, Simple Tokenizer." << endl;
+}
