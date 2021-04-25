5d3b8ab129bb9cf0010ddec1fb7b6ae52933c576
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2021-04-24 20:27:21 -0800

    add: template read() and write() files

---
+++ hw5/template/myreadwrite.cpp
@@ -0,0 +1,4 @@
+// myreadwrite.cpp
+// Solomon Himelbloom
+// 23 April 2021
+// Template read() and write() for CS 202.
---
+++ hw5/template/myreadwrite.hpp
@@ -0,0 +1,37 @@
+// myreadwrite.cpp
+// Solomon Himelbloom
+// 23 April 2021
+// Template read() and write() for CS 202.
+
+#include <iostream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <ostream>
+#include <fstream>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::fstream;
+using std::ifstream;
+using std::map;
+using std::ofstream;
+using std::reverse;
+using std::sort;
+using std::string;
+using std::vector;
+
+#ifndef TEMPLATE_MYREADWRITE_HPP
+#define TEMPLATE_MYREADWRITE_HPP
+
+template<typename Test>
+void myRead(ifstream& ifile, Test& reading) {
+    ifile.read(reinterpret_cast<char *>(&reading), sizeof(Test));
+}
+
+template <typename Test>
+void myWrite(ofstream& ofile, const Test &writing) {
+    ofile.write(reinterpret_cast<const char*>(&writing), sizeof(Test));
+}
+
+#endif //TEMPLATE_MYREADWRITE_HPP
