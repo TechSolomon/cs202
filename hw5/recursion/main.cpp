bab9e627155875ffc08fe955b01885dd93066021
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2021-04-24 22:53:57 -0800

    add: prep for Ackerman's Function Analysis

--- hw5/recursion/main.cpp
+++ hw5/recursion/main.cpp
@@ -44,5 +44,13 @@ int main() {
     std::cout << fib_loop(25) << std::endl;
 
     std::cout << ">> ack(n): " << std::endl;
+
+    std::cout << ack(0, 1) << std::endl;
+    std::cout << ack(1, 2) << std::endl;
+    std::cout << ack(2, 3) << std::endl;
+    std::cout << ack(3, 4) << std::endl;
+    std::cout << ack(4, 5) << std::endl;
+    std::cout << ack(6, 7) << std::endl;
+
     return 0;
 }
