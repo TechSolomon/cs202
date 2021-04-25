f5949f7c676f9febe3ea77ab0be48e7aaa3419cc
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2021-04-24 23:02:03 -0800

    remove: extraneous comments (recursion)
    Adds context for limited number computation.

--- hw5/recursion/main.cpp
+++ hw5/recursion/main.cpp
@@ -11,46 +11,26 @@ int main() {
 
     std::cout << ">> fib(0, 1, 2, 10, 25): " << std::endl;
 
-    // 0
     std::cout << fib(0) << std::endl;
-
-    // 1
     std::cout << fib(1) << std::endl;
-
-    // 2
     std::cout << fib(2) << std::endl;
-
-    // 10
     std::cout << fib(10) << std::endl;
-
-    // 25
     std::cout << fib(25) << std::endl;
 
     std::cout << ">> fib_loop(0, 1, 2, 10, 25): " << std::endl;
 
-    // 0
     std::cout << fib_loop(0) << std::endl;
-
-    // 1
     std::cout << fib_loop(1) << std::endl;
-
-    // 2
     std::cout << fib_loop(2) << std::endl;
-
-    // 10
     std::cout << fib_loop(10) << std::endl;
-
-    // 25
     std::cout << fib_loop(25) << std::endl;
 
-    std::cout << ">> ack(n): " << std::endl;
+    std::cout << ">> ack(m, n) where m = [0,3]; n = [1,4]: " << std::endl;
 
     std::cout << ack(0, 1) << std::endl;
     std::cout << ack(1, 2) << std::endl;
     std::cout << ack(2, 3) << std::endl;
     std::cout << ack(3, 4) << std::endl;
-    std::cout << ack(4, 5) << std::endl;
-    std::cout << ack(6, 7) << std::endl;
 
     return 0;
 }
